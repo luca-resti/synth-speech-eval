34e2819d4a40052215b601b4fbf74ad457a2179a
benheritage <ben.heritage@york.ac.uk>
2026-03-12 14:50:39 +0000

    - tidied pipeline for sq_ast prediction - outputting the resulting dataframe to the output folder

--- eval.py
+++ eval.py
@@ -1,6 +1,9 @@
 from models import sq_ast_mod
 import sys
+import os
 import yaml
+import pandas as pd
+from datetime import datetime
 
 if __name__ == "__main__":
 
@@ -12,4 +15,24 @@ if __name__ == "__main__":
     with open(config_path, 'r') as f:
         config_file = yaml.safe_load(f)
 
-    sq_ast_mod.sq_ast_fw(config_file)
+    output_dir = config_file["output_dir"] + "/" + datetime.now().strftime("%Y%m%d_%H%M%S") + "/"
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    output_ind_csv_path = os.path.join(output_dir, "output_individual.csv")
+
+    output_ind_df = pd.DataFrame(columns=["file_path", "sq_mos", "sq_noi", "sq_dis", "sq_col", "sq_loud"])
+
+    sq_ast_pred, attention_flows, fbank_lengths, audio_files_txt = sq_ast_mod.sq_ast_fw(config_file)
+
+    for i, (file_idx, file_path) in enumerate(audio_files_txt):
+        output_ind_df.loc[i] = {
+            "index": i,
+            "file_path": file_path,
+            "sq_mos": sq_ast_pred[i, 0],
+            "sq_noi": sq_ast_pred[i, 1],
+            "sq_dis": sq_ast_pred[i, 2],
+            "sq_col": sq_ast_pred[i, 3],
+            "sq_loud": sq_ast_pred[i, 4]
+        }
+
+    output_ind_df.to_csv(output_ind_csv_path, index=False)
--- models/sq_ast_mod.py
+++ models/sq_ast_mod.py
@@ -16,6 +16,12 @@ import numpy as np
 
 torch.multiprocessing.set_sharing_strategy('file_system')
 
+
+ALL_DIMS = ["mos", "noi", "dis", "col", "loud"]
+COL_IDX = {d: i for i, d in enumerate(ALL_DIMS)}
+SALIENCY_INTERP_SIZE = (128, 1024)
+
+
 class ASTXL(torch.nn.Module):
     """Model-XL with individual AST for each dimension."""
 
@@ -100,63 +106,18 @@ def get_expected_fbank_shape(waveform, sample_rate):
     return (num_mel_bins, num_frames)
 
 
-def tensor_attention_flow(attn_maps):
-    """
-    Approximates Attention Flow using tensor operations.
-    attn_maps: [layers, seq_len, seq_len]
-    """
-    num_layers, seq_len, _ = attn_maps.shape
-
-    I = torch.eye(seq_len).to(attn_maps.device)
-    A_adj = 0.5 * attn_maps + 0.5 * I # add identity for residual connections
+def prepare_dataloader(data_dir, wav_path, db_mean, db_std, bs, num_workers):
     
-    cls_flow = A_adj[0, 0, :] # CLS attention only!
-    for i in range(1, num_layers):
-        cls_flow = torch.min(cls_flow, A_adj[i]).sum(dim=-1)
-        cls_flow /= cls_flow.sum() # renormalise
-    return cls_flow[2:] # remove <CLS> and <DISTILL>
-
-
-ALL_DIMS = ["mos", "noi", "dis", "col", "loud"]
-COL_IDX = {d: i for i, d in enumerate(ALL_DIMS)}
-
-
-def sq_ast_fw(config_file):
-    
-    config_sq = config_file["sq_ast"]
-
-    current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
-
-    output_dir = config_sq["output_dir"]
-    dims = ALL_DIMS
-    bs = int(1)
-    num_workers = int(0)
-
-    if str(config_sq["path"]).endswith('.wav'): 
-        wav_path = config_sq["path"]
-        data_dir = None
-    else: 
-        data_dir = config_sq["path"]
-        wav_path = None
-
-    weights_path = "./" # Change this before packaging - perhaps put in same dir?
-    db_mean = -10.25446422 # calculated from the validation datasets from July 2025
-    db_std = 4.205750774 # calculated from the validation datasets from July 2025
-    
-    if config_sq["device"] == "gpu" and torch.cuda.is_available():
-        device = "cuda"
-    else:
-        device = "cpu"
+    fbank_lengths = []
+    audio_files_txt = []
 
     dtype_dict = {
-            'db': str,
-            'file_path': str,
-            'file_num': float,
-            'db_mean': float,
-            'db_std': float
-        }
-
-    fbank_lengths = []
+        'db': str,
+        'file_path': str,
+        'file_num': float,
+        'db_mean': float,
+        'db_std': float
+    }
 
     if data_dir:
         db_name = os.path.basename(data_dir)
@@ -173,6 +134,7 @@ def sq_ast_fw(config_file):
         for data_index in range(len(file_paths)):
             data_file = file_paths[data_index]
             audio, sample_rate = torchaudio.load(os.path.join(data_dir, data_file))
+            audio_files_txt.append([data_index, os.path.join(data_dir, data_file)])
             fbank_lengths.append([data_index, get_expected_fbank_shape(audio, sample_rate)[1]])
    
         ds = ASTVal(df, data_dir, db_mean, db_std)
@@ -191,6 +153,7 @@ def sq_ast_fw(config_file):
         })
 
         audio, sample_rate = torchaudio.load(os.path.basename(wav_path))
+        audio_files_txt.append([0, os.path.basename(wav_path)])
         fbank_lengths.append([0, get_expected_fbank_shape(audio, sample_rate)[1]])
 
         ds = ASTVal(df, os.path.dirname(wav_path), db_mean, db_std)
@@ -201,32 +164,96 @@ def sq_ast_fw(config_file):
         shuffle=False,
         num_workers=num_workers
     )
-    
-    models_by_dim = {}
-    for dim in dims:  # dims comes from argparse
-        model = ASTXL()
-        state = torch.load(f"models/weights/{dim}.pth", map_location=torch.device(device), weights_only=True)
-        model.load_state_dict(state)
-        model.to(device)
-        model.eval()
-        models_by_dim[dim] = model
 
+    return dl, fbank_lengths, audio_files_txt
+
+
+def get_scaled_saliency_map(attn_map, saliency_interp_method):
+    attn_map = torch.tensor(attn_map).unsqueeze(0).unsqueeze(0)
+    return F.interpolate(attn_map, size=SALIENCY_INTERP_SIZE, mode=saliency_interp_method)
+    
+
+def tensor_attention_flow(attn_maps):
+    """
+    Approximates Attention Flow using tensor operations.
+    attn_maps: [layers, seq_len, seq_len]
+    """
+    num_layers, seq_len, _ = attn_maps.shape
+
+    I = torch.eye(seq_len).to(attn_maps.device)
+    A_adj = 0.5 * attn_maps + 0.5 * I # add identity for residual connections
+    
+    cls_flow = A_adj[0, 0, :] # CLS attention only!
+    for i in range(1, num_layers):
+        cls_flow = torch.min(cls_flow, A_adj[i]).sum(dim=-1)
+        cls_flow /= cls_flow.sum() # renormalise
+    return cls_flow[2:] # remove <CLS> and <DISTILL>
+
+
+def get_pred_attn(dims, dl, device):
+    
     attention_flows = torch.Tensor(np.zeros(shape=(dl.__len__(), len(dims), 12, 101)))
     predictions = torch.Tensor(np.zeros(shape=(dl.__len__(), len(dims))))
 
     for dim_index in range(len(dims)):
         dim = dims[dim_index]
-        model = models_by_dim[dim].eval()
-
-        for b, (index, batch_features) in enumerate(dl):
             
-            batch_features = batch_features.float().to(device)
+        with torch.no_grad():
+            model = ASTXL()
+            model.load_state_dict(torch.load(f"models/weights/{dim}.pth", map_location=torch.device(device), weights_only=True))
+            model.to(device)
+            model.eval()
 
-            pred, attentions = model(batch_features, output_attentions=True)
+            for _, (index, batch_features) in enumerate(dl):
+                
+                batch_features = batch_features.float().to(device)
 
-            predictions[index, dim_index] = pred # store prediction for this SQ dimension
+                pred, attentions = model(batch_features, output_attentions=True)
 
-            attention_flow = tensor_attention_flow(attentions[0][0].cpu().detach())
-            attention_flows[index, dim_index, :, :] = attention_flow.reshape(12, 101) # store downscaled attention flow    
+                predictions[index, dim_index] = 4*pred + 1 # store prediction for this SQ dimension
 
-    pass
+                attention_flow = tensor_attention_flow(attentions[0][0].cpu().detach())
+                attention_flows[index, dim_index, :, :] = attention_flow.reshape(12, 101)
+
+    return predictions, attention_flows
+
+
+def sq_ast_fw(config_file):
+
+    current_time = datetime.now()
+    output_dir = config_file["output_dir"]
+    dims = ALL_DIMS
+    bs = int(1)
+    num_workers = int(0)
+
+    # check whether is a single file or a directory and set paths accordingly
+    if str(config_file["path"]).endswith('.wav'): 
+        wav_path = config_file["path"]
+        data_dir = None
+    else: 
+        data_dir = config_file["path"]
+        wav_path = None
+
+    db_mean = -10.25446422 # calculated from the validation datasets from July 2025
+    db_std = 4.205750774 # calculated from the validation datasets from July 2025
+    
+    # get device from config and check if GPU is available
+    if config_file["device"] == "gpu" and torch.cuda.is_available():
+        device = "cuda"
+    else:
+        device = "cpu"
+
+    # create dataset for sq_ast
+    dl, fbank_lengths, audio_files_txt = prepare_dataloader(data_dir, wav_path, db_mean, db_std, bs, num_workers)
+    
+    # get the predictions and attention flows for each dimension
+    predictions, attention_flows = get_pred_attn(dims, dl, device)
+
+    print(f"SQ_AST inference completed in time: {datetime.now() - current_time}")
+
+    return (
+        predictions.cpu().numpy(), 
+        attention_flows.cpu().numpy(), 
+        fbank_lengths, 
+        audio_files_txt
+    )
